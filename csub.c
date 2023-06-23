b9081ca4ee17b32272b69c30fa16c557976345ab
Awantika bijwe <30284191+AwantikaBijwe@users.noreply.github.com>
2023-06-23 17:45:52 +0530

    Update csub.c

--- csub.c
+++ csub.c
@@ -2,9 +2,11 @@
 int main() {    
 
     int number1, number2, sum;
+    number1=12;
+    number2=12;
     
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+   // printf("Enter two integers: ");
+   // scanf("%d %d", &number1, &number2);
 
     // calculate the sub
     sum = number1 - number2;      
